a32ad996ad49b6c40050650b4212e0c076a37f01
vivekiteos <vivek.s@iteos.in>
2024-04-18 19:15:48 +0530

    fix: connection

--- app/config.py
+++ app/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     USERNAME:str = "v_portal"
     PASSWORD:str = "QUOnEypcpIGqo6N"
     PORT: int = 5432
-    SQLALCHEMY_DATABASE_URI: str ="postgresql://v_portal:QUOnEypcpIGqo6N@182.16.16.24:5432/VendorPortal"
+    SQLALCHEMY_DATABASE_URI: str ="postgresql://postgres:postgres@postgres.cvowww668jr9.ap-south-1.rds.amazonaws.com/postgres"
+    # SQLALCHEMY_DATABASE_URI: str ="postgresql://v_portal:QUOnEypcpIGqo6N@182.16.16.24:5432/VendorPortal"
 
 settings= Settings()
